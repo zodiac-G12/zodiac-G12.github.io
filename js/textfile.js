9fe7f71aea2677bfa4ffc47355acc52896fdb717
zodiac-G12 <zodiac@zodiac-ThinkPad-E450>
2017-08-02 06:30:20 +0900

    test2

--- js/textfile.js
+++ js/textfile.js
@@ -1,6 +1,7 @@
 function textadd(){
   $(function(){
     var html = window.location.href.split("\.")[2].split("/")[1] + '.txt';
+    alert(html);
     $.ajax({
       url: 'https://zodiac-g12.github.io/' + html,
       type: 'get',
