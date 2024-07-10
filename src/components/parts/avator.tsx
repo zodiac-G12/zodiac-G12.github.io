1ff8c07158843fcb86502e7a9857523c89c7dbd3
zodiac-G12 <12181838+zodiac-G12@users.noreply.github.com>
2024-07-11 07:05:29 +0900

    feat: add demo build and add glitch avator

--- src/components/parts/avator.tsx
+++ src/components/parts/avator.tsx
@@ -1,11 +1,11 @@
 export const Avator = () => {
   return (
-    <img
-      class="rounded-full"
-      width="150px"
-      height="150px"
-      alt="my avator"
-      src="https://avatars1.githubusercontent.com/u/12181838?s=460&av=4"
-    />
+    <div class="glitch rounded-full h-[150px] w-[150px]">
+      <div class="glitch__img" />
+      <div class="glitch__img" />
+      <div class="glitch__img" />
+      <div class="glitch__img" />
+      <div class="glitch__img" />
+    </div>
   );
 };
